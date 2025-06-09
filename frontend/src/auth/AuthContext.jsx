266c70beab432403d2afad7530294b591c6d284a
FranciscoAR <95307098+Lamb346@users.noreply.github.com>
2025-06-09 17:19:01 -0300

    Update AuthContext.jsx
    refactor: changing fake user

--- frontend/src/auth/AuthContext.jsx
+++ frontend/src/auth/AuthContext.jsx
@@ -6,7 +6,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const fakeUser = {
-    _id: '2222',
+    _id: '1',
     email: 'teste@teste.com',
     name: 'Usuário Teste',
   };
